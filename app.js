05c0ea1bc466f20b349b6a621493b6ddc9fa2359
Your Name <you@example.com>
2019-03-27 19:02:11 -0400

    Pushing project to github

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+const bookRouter = express.Router();
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    res.send('hello books');
+  });
+  
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send("Hello single book");
+  });
+
+app.use('/books', bookRouter);
+app.get('/',
+ (req, res) => {
+  res.render(
+    'index',
+    {
+      nav:
+        [
+          { link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }
+        ],
+      title: 'Library',
+    }
+  );
+});
+
+app.listen(port, () => {
+  debug(`running on port ${chalk.magenta(port)}`);
+});
