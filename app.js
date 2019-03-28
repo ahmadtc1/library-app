a64bf4217825d8484a5c639d661b4bf4b879e004
Your Name <you@example.com>
2019-03-27 22:26:48 -0400

    Split book routing into separate file for cleaner code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const port = process.env.PORT || 3000;
 const app = express();
-const bookRouter = express.Router();
+const bookRouter = require('./src/routes/bookRoutes');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -15,63 +15,6 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
-const books = [
-  {
-    title: 'War and Peace',
-    genre: 'Historical Ficiton',
-    author: 'Lev Nikolayevich Tolstoy',
-    read: false
-  },
-  {
-    title: 'War and Peace2',
-    genre: 'Historical Ficiton2',
-    author: 'Lev Nikolayevich Tolstoy2',
-    read: false
-  },
-  {
-    title: 'War and Peace3',
-    genre: 'Historical Ficiton3',
-    author: 'Lev Nikolayevich Tolstoy3',
-    read: false
-  },
-  {
-    title: 'War and Peace4',
-    genre: 'Historical Ficiton4',
-    author: 'Lev Nikolayevich Tolstoy4',
-    read: false
-  },
-  {
-    title: 'War and Peace5',
-    genre: 'Historical Ficiton5',
-    author: 'Lev Nikolayevich Tolstoy5',
-    read: false
-  },
-  {
-    title: 'War and Peace6',
-    genre: 'Historical Ficiton6',
-    author: 'Lev Nikolayevich Tolstoy6',
-    read: false
-  },
-];
-
-
-bookRouter.route('/')
-  .get((req, res) => {
-    res.render(
-      'books',
-      {
-        nav: [{ link: '/books', title: 'Books' },
-        { link: '/authors', title: 'Authors' }],
-        title: 'Library',
-        books
-      });
-  });
-
-bookRouter.route('/single')
-  .get((req, res) => {
-    res.send("Hello single book");
-  });
 
 app.use('/books', bookRouter);
 app.get('/',
