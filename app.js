334a51317a2aa822a58910331c957d71601fa087
Your Name <you@example.com>
2019-03-27 22:21:06 -0400

    Using data to populate webpage

--- app.js
+++ app.js
@@ -16,6 +16,45 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Ficiton',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'War and Peace2',
+    genre: 'Historical Ficiton2',
+    author: 'Lev Nikolayevich Tolstoy2',
+    read: false
+  },
+  {
+    title: 'War and Peace3',
+    genre: 'Historical Ficiton3',
+    author: 'Lev Nikolayevich Tolstoy3',
+    read: false
+  },
+  {
+    title: 'War and Peace4',
+    genre: 'Historical Ficiton4',
+    author: 'Lev Nikolayevich Tolstoy4',
+    read: false
+  },
+  {
+    title: 'War and Peace5',
+    genre: 'Historical Ficiton5',
+    author: 'Lev Nikolayevich Tolstoy5',
+    read: false
+  },
+  {
+    title: 'War and Peace6',
+    genre: 'Historical Ficiton6',
+    author: 'Lev Nikolayevich Tolstoy6',
+    read: false
+  },
+];
+
 
 bookRouter.route('/')
   .get((req, res) => {
@@ -24,7 +63,8 @@ bookRouter.route('/')
       {
         nav: [{ link: '/books', title: 'Books' },
         { link: '/authors', title: 'Authors' }],
-        title: 'Library'
+        title: 'Library',
+        books
       });
   });
 
