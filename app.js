a1e89707422964db90c01ee830ccaa39b03d7ad4
Your Name <you@example.com>
2019-03-31 11:23:23 -0400

    Adding user POST route:

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 const sql = require('mssql');
-
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -29,8 +29,11 @@ const nav = [
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
@@ -39,6 +42,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter)
+app.use('/auth', authRouter);
 
 
 
