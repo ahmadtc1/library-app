87d27a9b1083c6a87ec8337be8e26a2b4f3272ed
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-01 21:02:14 -0400

    Implementing API call to goodreads

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,6 +1,27 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+
+const parser = xml2js.Parser({ explicitArray: false });
+
 function goodReadsService() {
-    function getBookById() {
+    function getBookById(id) {
         return new Promise((resolve, reject) => {
+            axios.get('https://www.goodreads.com/book/show/656.xml?key=zU3O4cJ1qHe0jzL8Br77w')
+                .then((response) => {
+                    parser.parseString(response.data, (err, result) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        else {
+                            console.log(result);
+                            resolve(result.GoodreadsResponse.book);
+                        }
+                    })
+                })
+                .catch((error) => {
+                    reject(error);
+                    console.log(error);
+                })
             resolve({ description: 'Our Description' });
         });
     }
